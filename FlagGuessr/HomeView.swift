aee2ca2ec792a2d0c23cc89fdc99e6728a079223
Lorenzo Ilardi <l.ilardi@reply.it>
2025-04-04 00:32:18 +0200

    Add flag assets

---
+++ FlagGuessr/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  FlagGuessr
+//
+//  Created by Lorenzo Ilardi on 03/04/25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    @State private var showingAlert = false
+
+       var body: some View {
+           Text("Hello, World!")
+       }
+}
+
+#Preview {
+    HomeView()
+}
