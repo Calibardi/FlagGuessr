5f87f90ae0d84c42e54fa0859fb09f7586141300
Lorenzo Ilardi <l.ilardi@reply.it>
2025-04-03 16:34:51 +0200

    Initial Commit

---
+++ FlagGuessr/FlagGuessrApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagGuessrApp.swift
+//  FlagGuessr
+//
+//  Created by Lorenzo Ilardi on 03/04/25.
+//
+
+import SwiftUI
+
+@main
+struct FlagGuessrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
