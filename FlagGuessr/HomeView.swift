eeea7d3aed544ce67cca5adf5d6cfaf428b8ac03
Lorenzo Ilardi <l.ilardi@reply.it>
2025-04-04 21:00:37 +0200

    Add score logic

--- FlagGuessr/HomeView.swift
+++ FlagGuessr/HomeView.swift
@@ -9,16 +9,21 @@ import SwiftUI
 
 /*
  
- 1. Add an @State property to store the user’s score, modify it when they get an answer right or wrong, then display it in the alert and in the score label.
- 2. When someone chooses the wrong flag, tell them their mistake in your alert message – something like “Wrong! That’s the flag of France,” for example.
+ 1. Add an @State property to store the user’s score, modify it when they get an answer right or wrong, then display it in the alert and in the score label. - Done
+ 2. When someone chooses the wrong flag, tell them their mistake in your alert message – something like “Wrong! That’s the flag of France,” for example. - Done
  3. Make the game show only 8 questions, at which point they see a final alert judging their score and can restart the game.
  
  */
 
 struct HomeView: View {
     @Environment(\.verticalSizeClass) private var verticalSizeClass
+    
     @State private var showingScore = false
     @State private var scoreTitle = ""
+    @State private var alertMessage = ""
+    @State private var score = 0
+    
+    // MARK: DATA
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
     
@@ -29,6 +34,7 @@ struct HomeView: View {
                 .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.7),
             ], center: .top, startRadius: 200, endRadius: 400)
             .ignoresSafeArea()
+            
             VStack {
                 Spacer()
                 Text("Guess the flag")
@@ -65,7 +71,7 @@ struct HomeView: View {
                 Spacer()
                 
                 ZStack {
-                    Text("Score: ???")
+                    Text("Score: \(score)")
                         .font(.title.bold())
                         .padding()
                 }
@@ -78,15 +84,18 @@ struct HomeView: View {
         }.alert(scoreTitle, isPresented: $showingScore) {
             Button("Continue", action: askQuestion)
         } message: {
-            Text("Your score is ???")
+            Text(alertMessage)
         }
     }
     
     private func didTapFlagButton(_ number: Int) {
         if number == correctAnswer {
             scoreTitle = "Correct!"
+            score += 1
+            alertMessage = "Your score is \(score)"
         } else {
             scoreTitle = "Wrong!"
+            alertMessage = "That is the flag of \(countries[number])"
         }
         
         showingScore = true
